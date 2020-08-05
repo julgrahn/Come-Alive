62bdae1ade8c3f986f2af58e1e01f7fed9dfa0e6
julgrahn <alexander.grahn@hotmail.se>
2020-08-05 22:48:32 +0200

    add enemy attack, player HP, enemy rotation

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,12 +7,12 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 10f;
+    [SerializeField] float turnSpeed = 5f;
 
 
     NavMeshAgent navMeshAgent;
     float distToTarg = Mathf.Infinity;
     bool isProvoked = false;
-    bool inRange = false;
 
 
     void Start()
@@ -37,6 +37,7 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
         if (distToTarg >= navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -50,12 +51,21 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackTarget()
     {
-        Debug.Log(name + " Attacking!" + target.name);
+        GetComponent<Animator>().SetBool("attack", true);
     }
 
     private void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("attack", false);
+        GetComponent<Animator>().SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
     }
 
     void OnDrawGizmosSelected()
