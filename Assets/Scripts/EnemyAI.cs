a3a2b15d02800a00e873076f9bf8f62984188f14
julgrahn <alexander.grahn@hotmail.se>
2020-08-05 04:12:35 +0200

    add enemy AI and chase range gizmo

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
+
+
     NavMeshAgent navMeshAgent;
+    float distToTarg = Mathf.Infinity;
+    
 
 
     void Start()
@@ -16,6 +21,18 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        OnDrawGizmosSelected();
+        distToTarg = Vector3.Distance(target.position, transform.position);
+        if (distToTarg <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 0, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
