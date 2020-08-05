f544985241037e1e2dc921c92764bbfbc9fa89da
julgrahn <alexander.grahn@hotmail.se>
2020-08-06 01:09:35 +0200

    remove crosshair in death canvas, make enemy move upon damage

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class DeathHandler : MonoBehaviour
 {
     [SerializeField] Canvas gameOverCanvas;
+    [SerializeField] Canvas gunReticleCanvas;
 
     private void Start()
     {
+        gunReticleCanvas.enabled = true;
         gameOverCanvas.enabled = false;
     }
 
     public void HandleDeath()
     {
+        GetComponent<RigidbodyFirstPersonController>().enabled = false;
+        GetComponentInChildren<Weapon>().enabled = false;
+        gunReticleCanvas.enabled = false;
         gameOverCanvas.enabled = true;
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
