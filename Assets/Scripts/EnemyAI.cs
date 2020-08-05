46da6ee327740023ec458854ce42fb0d10219318
julgrahn <alexander.grahn@hotmail.se>
2020-08-05 04:26:19 +0200

    add method for enemy to start attacking player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -11,7 +11,8 @@ public class EnemyAI : MonoBehaviour
 
     NavMeshAgent navMeshAgent;
     float distToTarg = Mathf.Infinity;
-    
+    bool isProvoked = false;
+    bool inRange = false;
 
 
     void Start()
@@ -21,17 +22,44 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        OnDrawGizmosSelected();
         distToTarg = Vector3.Distance(target.position, transform.position);
-        if (distToTarg <= chaseRange)
+
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if (distToTarg <= chaseRange)
+        {
+            isProvoked = true;
+            ChaseTarget();
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distToTarg >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distToTarg <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " Attacking!" + target.name);
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
     }
 
     void OnDrawGizmosSelected()
     {
-        // Display the explosion radius when selected
         Gizmos.color = new Color(1, 0, 0, 0.75F);
         Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
