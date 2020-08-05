b84cc3ee465b665f5a0a2a6d83f7f25d66b761be
julgrahn <alexander.grahn@hotmail.se>
2020-08-05 05:08:03 +0200

    add weapon, crosshair and raycast

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float bulletRange = 100f;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, bulletRange);
+    }
+}
