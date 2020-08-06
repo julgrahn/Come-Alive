39b2e87cf4faeb9b496710dc81a861bb17d7eab1
julgrahn <alexander.grahn@hotmail.se>
2020-08-06 02:03:38 +0200

    add zoom in function for weapon, change FOV

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomOutFOV = 60f;
+    [SerializeField] float zoomInFOV = 20f;
+
+    bool zoomedInToggle = false;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomOutFOV;
+            }
+        }
+    }
+
+}
