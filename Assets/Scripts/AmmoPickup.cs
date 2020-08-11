50803e5e6a01754772217d1a78ab5ae8b8f2361c
julgrahn <alexander.grahn@hotmail.se>
2020-08-11 04:19:26 +0200

    add basic pickup, import zombie model asset

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("Pickup.");
+            FindObjectOfType<Ammo>().AddCurrentAmmo(ammoType, ammoAmount);
             Destroy(gameObject);
         }
     }
