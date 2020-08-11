a76d4b121d8f6160a458fa03114a0f5e49919b11
julgrahn <alexander.grahn@hotmail.se>
2020-08-11 02:58:49 +0200

    fix bug with weapon zoom

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -16,25 +16,39 @@ public class WeaponZoom : MonoBehaviour
 
     bool zoomedInToggle = false;
 
+    private void OnDisable()
+    {
+        ZoomOut();
+    }
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(1))
         {
             if(zoomedInToggle == false)
             {
-                zoomedInToggle = true;
-                fpsCamera.fieldOfView = zoomInFOV;
-                fpsController.mouseLook.XSensitivity = mouseSenseZoomIn;
-                fpsController.mouseLook.YSensitivity = mouseSenseZoomIn;
+                ZoomIn();
             }
             else
             {
-                zoomedInToggle = false;
-                fpsCamera.fieldOfView = zoomOutFOV;
-                fpsController.mouseLook.XSensitivity = mouseSenseZoomOut;
-                fpsController.mouseLook.YSensitivity = mouseSenseZoomOut;
+                ZoomOut();
             }
         }
     }
 
+    private void ZoomIn()
+    {
+        zoomedInToggle = true;
+        fpsCamera.fieldOfView = zoomInFOV;
+        fpsController.mouseLook.XSensitivity = mouseSenseZoomIn;
+        fpsController.mouseLook.YSensitivity = mouseSenseZoomIn;
+    }
+
+    private void ZoomOut()
+    {
+        zoomedInToggle = false;
+        fpsCamera.fieldOfView = zoomOutFOV;
+        fpsController.mouseLook.XSensitivity = mouseSenseZoomOut;
+        fpsController.mouseLook.YSensitivity = mouseSenseZoomOut;
+    }
 }
