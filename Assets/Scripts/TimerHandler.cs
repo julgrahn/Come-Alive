0b57621ae197c734a36018e1d247c7daea76499f
julgrahn <alexander.grahn@hotmail.se>
2020-08-23 04:52:50 +0200

    dont know what im doing...

--- Assets/Scripts/TimerHandler.cs
+++ Assets/Scripts/TimerHandler.cs
@@ -10,16 +10,18 @@ namespace HighScore
     public class TimerHandler : MonoBehaviour
     {
         public Text timerText;
+        public Text scoreText;
         public Text highScoreText;
         private float startTime;
         public bool stopped;
-        public string score;
-        public float highscore;
+        public int highscore;
+        public float score;
+
 
         void Start()
         {
             startTime = Time.time;
-            score = "0";
+            score = 0;
         }
 
         void Update()
@@ -31,23 +33,23 @@ namespace HighScore
         public void TimeCounter()
         {
             float t = Time.time - startTime;
-            PlayerPrefs.SetFloat("firstScore", t);
-            if(PlayerPrefs.GetFloat("firstScore") <= highscore)
+
+            score += Time.deltaTime * 5;
+            highscore = (int)score;
+            scoreText.text = highscore.ToString();
+
+            if(PlayerPrefs.GetInt("score") <= highscore)
             {
-                highscore = PlayerPrefs.GetFloat("firstScore");
+                PlayerPrefs.SetInt("score", highscore);
             }
+            highScoreText.text = PlayerPrefs.GetInt("score").ToString();
+
 
             string minutes = ((int)t / 60).ToString();
             string seconds = (t % 60).ToString("f0");
             timerText.text = minutes + ":" + seconds;
 
-            score = timerText.ToString();
 
-            if(t <= highscore)
-            {
-                PlayerPrefs.SetString("score", highscore.ToString());
-            }
-            highScoreText.text = PlayerPrefs.GetString("score").ToString();
         }
 
         public void TimerStopper()
